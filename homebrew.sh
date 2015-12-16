28f601b559d4b3dc23c0c23588f63c6c97c22483
isuzuki <isuzuki@suzukiikuonoAir.lan>
2015-12-17 04:26:21 +0900

    add homebrew.sh and vim shellcheck config

---
+++ homebrew.sh
@@ -0,0 +1,77 @@
+#! /bin/bash
+
+# homebrewがインストール済みではない場合、インストールする
+if test ! "$(which brew)"; then
+	echo "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew
+printf "Update? [Y/n]: " && read -r answer
+if test "$answer" = "Y"; then
+	brew update
+fi
+
+# Upgrade homebrew
+printf "Upgrade? [Y/n]: " && read -r answer
+if test "$answer" = "Y"; then
+	brew upgrade
+fi
+
+# Add Repository
+brew tap laurent22/massren
+
+# Formulas
+formulas=(
+
+	# Shell
+	zsh
+	zsh-completions
+	paco
+
+	# Multiplexe
+	tmux
+	reattach-to-user-namespace
+
+	# Tools
+	ag
+	nkf
+	massren
+	pstree
+	tree
+	wget
+
+	# Git
+	git
+
+	# Languages
+	go
+
+	# Languages syntax
+	shellcheck
+
+	# Operations
+	ansible
+
+)
+
+echo "Installing formulas..."
+brew install "${formulas[@]}" && brew cleanup
+
+# Install Casks
+brew install caskroom/cask/brew-cask
+
+# Applications
+applications=(
+
+	# Chat
+	slack
+
+	# VM
+	virtualbox
+	vagrant
+)
+
+# Install apps to /Applications
+echo "installing applications..."
+brew cask install --appdir="/Applications" "${applications[@]}"
